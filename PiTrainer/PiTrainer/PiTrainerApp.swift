79521f3ac1ed5dbd3c2028fda6dd2c3cc45f51db
Alexandre SAHUT <alexandre@MacBook-Pro-M5.local>
2026-01-10 23:40:09 +0100

    chore: bootstrap iOS SwiftUI project

---
+++ PiTrainer/PiTrainer/PiTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiTrainerApp.swift
+//  PiTrainer
+//
+//  Created by Alexandre SAHUT on 10/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct PiTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
